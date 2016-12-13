4d1d51c1e82b6a9152e84925e54ca3cb14e91623
Mehmet Duran <mehmet.duran@gmail.com>
2016-12-13 16:31:39 +0000

    More

--- build/generate.js
+++ build/generate.js
@@ -1,11 +1,45 @@
 var nunjucks = require('nunjucks'),
     fs = require('fs');
 
+function getFilename( catname, testname ){
+    var filename = [catname.toLowerCase(), testname.toLowerCase()]
+                      .join('-')
+                      .replace(/[^a-z0-9\-\ ]/, '')
+                      .replace('/', ' ')
+                      .replace(/\s+/g, '-')
+                      .replace(/-+/g, '-');
+
+    return filename;
+}
+
 var testsFile = fs.readFileSync('../tests.json').toString();
 var tests = JSON.parse(testsFile);
 
 nunjucks.configure('templates');
 
+// Generate index
+
 var indexout = nunjucks.render('index.html', {tests: tests});
 
 fs.writeFileSync('out/index.html', indexout, 'utf8');
+
+// Generate individual tests
+
+for( catname in tests ){
+  for( testname in tests[catname] ){
+    var testObj = tests[catname][testname];
+
+    var filename = getFilename( catname, testname );
+
+    var filecontent = nunjucks.render('single-test.html', {
+      testname: testname,
+      example: testObj.example
+    });
+
+    fs.writeFileSync('out/tests/' + filename + ".html", filecontent, 'utf8');    
+  }
+}
+
+// Generate results
+
+
